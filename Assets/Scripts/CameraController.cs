d53e5e34f1b92825cb93deb24440cef323141864
Peter Bui <peterbui27@gmail.com>
2024-02-28 18:38:48 -0500

    Added Dash and cleaned up code for PlayerController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,9 @@ public class CameraController : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
-
     public bool useOffsetValues;
+
+    public float rotateSpeed;
 
     void Start()
     {
@@ -20,7 +21,19 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        transform.position = player.position - offset;
+        // get the X position of the mouse & rotates the player
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        player.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        player.Rotate(vertical, 0, 0);
+
+        // moves the camera based on the current rotation of the player and the original offset
+        float desiredYAngle = player.eulerAngles.y;
+        float desiredXAngle = player.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = player.position - (rotation * offset);
+
         transform.LookAt(player);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,94 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public float dashForce;
+    public float dashDuration;
+    public float dashCooldown;
     public CharacterController controller;
     public Vector3 moveDirection;
     public float gravityScale;
 
-    void Start()
+    [SerializeField] private TrailRenderer tr;
+
+    private bool isDashing;
+    private bool canDash = true;
+
+    private void Start()
     {
-        controller = GetComponent<CharacterController>();
+        tr.enabled = false;
     }
 
     void Update()
     {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        HandleMovementInput();
 
         if (controller.isGrounded)
         {
-            moveDirection.y = 0f;
-            if (Input.GetButtonDown("Jump"))
-            {
-                moveDirection.y = jumpForce;
-            }
+            HandleJumpInput();
         }
 
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        ApplyGravity();
+
+
+        // Handle dash input
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        // Move the character
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void HandleMovementInput()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+    }
+
+    void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+    }
+
+    void ApplyGravity()
+    {
+        moveDirection.y += Physics.gravity.y * gravityScale * Time.deltaTime;
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false;
+
+        // Enable the TrailRenderer
+        tr.enabled = true;
+
+        // Store the original move speed
+        float originalMoveSpeed = moveSpeed;
+
+        // Increase move speed for the dash duration
+        moveSpeed += dashForce;
+
+        // Wait for the dash duration
+        yield return new WaitForSeconds(dashDuration);
+
+        // Reset the move speed
+        moveSpeed = originalMoveSpeed;
+
+        // Disable the TrailRenderer
+        tr.enabled = false;
+
+        // Allow dashing again after cooldown
+        yield return new WaitForSeconds(dashCooldown);
+
+        canDash = true;
+        isDashing = false;
     }
 }
