7df2182a965c4dae4a42b11e23f9c77212f2cdca
RMEfan <mrcheesyman1337@gmail.com>
2024-03-01 01:07:53 -0800

    Limit the camera's range of motion
    Limits how far the camera can rotate up and down, as well as how far it can move downwards.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,7 +10,8 @@ public class CameraController : MonoBehaviour
 
     public float rotateSpeed;
     public Transform cameraPivot;
-
+    public float maxLookUp;
+    public float maxLookDown;
     void Start()
     {
         if (!useOffsetValues)
@@ -21,18 +22,30 @@ public class CameraController : MonoBehaviour
         cameraPivot.transform.position = player.transform.position;
         cameraPivot.transform.parent = player.transform;
         Cursor.lockState = CursorLockMode.Locked;
+        //maxLookDown = 70f;
+        //maxLookUp = 70f;
     }
 
     void Update()
     {
         
         // get the X position of the mouse & rotates the pivot
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        float horizontal = Input.GetAxisRaw("Mouse X") * rotateSpeed;
         player.Rotate(0, horizontal, 0);
 
         // get the Y position of the mouse & rotates the pivot
-        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        float vertical = -Input.GetAxisRaw("Mouse Y") * rotateSpeed;
         cameraPivot.Rotate(vertical, 0, 0);
+
+        // limit range of motion of the camera - rotation up and down
+        if (cameraPivot.rotation.eulerAngles.x > maxLookDown && cameraPivot.rotation.eulerAngles.x < 180f)
+        {
+            cameraPivot.rotation = Quaternion.Euler(maxLookDown, 0, 0);
+        }
+        if (cameraPivot.rotation.eulerAngles.x > 180f && cameraPivot.rotation.eulerAngles.x < 360f - maxLookUp)
+        {
+            cameraPivot.rotation = Quaternion.Euler(360f-maxLookUp, 0, 0);
+        }
 
         // moves the camera based on the current rotation of the player and the original offset
         float desiredYAngle = player.eulerAngles.y;
@@ -40,10 +53,10 @@ public class CameraController : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = player.position - (rotation * offset);
 
-        if (transform.position.y < player.position.y - .5f)
-        {
-            int y = 1;
-        }
+        // limit range of motion of the camera - y position 
+        // TODO: camera will still clip through objects. may or may not need to be fixed
+        float cappedY = Mathf.Max(player.position.y - .5f, transform.position.y);
+        transform.position = new Vector3(transform.position.x, cappedY, transform.position.z);
         transform.LookAt(player);
     }
 }
