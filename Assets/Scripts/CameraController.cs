bd82e3b2fea4222159558514b349547fe0b4c2d3
Peter Bui <peterbui27@gmail.com>
2024-03-01 18:25:03 -0500

    Added a pause menu, and a button to restart the level.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -28,35 +28,41 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        
-        // get the X position of the mouse & rotates the pivot
-        float horizontal = Input.GetAxisRaw("Mouse X") * rotateSpeed;
-        player.Rotate(0, horizontal, 0);
-
-        // get the Y position of the mouse & rotates the pivot
-        float vertical = -Input.GetAxisRaw("Mouse Y") * rotateSpeed;
-        cameraPivot.Rotate(vertical, 0, 0);
-
-        // limit range of motion of the camera - rotation up and down
-        if (cameraPivot.rotation.eulerAngles.x > maxLookDown && cameraPivot.rotation.eulerAngles.x < 180f)
+        if (PauseMenu.isPaused)
         {
-            cameraPivot.rotation = Quaternion.Euler(maxLookDown, 0, 0);
+
         }
-        if (cameraPivot.rotation.eulerAngles.x > 180f && cameraPivot.rotation.eulerAngles.x < 360f - maxLookUp)
+        else
         {
-            cameraPivot.rotation = Quaternion.Euler(360f-maxLookUp, 0, 0);
+            // get the X position of the mouse & rotates the pivot
+            float horizontal = Input.GetAxisRaw("Mouse X") * rotateSpeed;
+            player.Rotate(0, horizontal, 0);
+
+            // get the Y position of the mouse & rotates the pivot
+            float vertical = -Input.GetAxisRaw("Mouse Y") * rotateSpeed;
+            cameraPivot.Rotate(vertical, 0, 0);
+
+            // limit range of motion of the camera - rotation up and down
+            if (cameraPivot.rotation.eulerAngles.x > maxLookDown && cameraPivot.rotation.eulerAngles.x < 180f)
+            {
+                cameraPivot.rotation = Quaternion.Euler(maxLookDown, 0, 0);
+            }
+            if (cameraPivot.rotation.eulerAngles.x > 180f && cameraPivot.rotation.eulerAngles.x < 360f - maxLookUp)
+            {
+                cameraPivot.rotation = Quaternion.Euler(360f - maxLookUp, 0, 0);
+            }
+
+            // moves the camera based on the current rotation of the player and the original offset
+            float desiredYAngle = player.eulerAngles.y;
+            float desiredXAngle = cameraPivot.eulerAngles.x;
+            Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+            transform.position = player.position - (rotation * offset);
+
+            // limit range of motion of the camera - y position 
+            // TODO: camera will still clip through objects. may or may not need to be fixed
+            float cappedY = Mathf.Max(player.position.y - .5f, transform.position.y);
+            transform.position = new Vector3(transform.position.x, cappedY, transform.position.z);
+            transform.LookAt(player);
         }
-
-        // moves the camera based on the current rotation of the player and the original offset
-        float desiredYAngle = player.eulerAngles.y;
-        float desiredXAngle = cameraPivot.eulerAngles.x;
-        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
-        transform.position = player.position - (rotation * offset);
-
-        // limit range of motion of the camera - y position 
-        // TODO: camera will still clip through objects. may or may not need to be fixed
-        float cappedY = Mathf.Max(player.position.y - .5f, transform.position.y);
-        transform.position = new Vector3(transform.position.x, cappedY, transform.position.z);
-        transform.LookAt(player);
     }
 }
