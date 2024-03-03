81af987da5c683de37950018ca3593efcd11148d
Peter Bui <peterbui27@gmail.com>
2024-03-03 15:58:04 -0500

    Added bite sound effect to collectible pickup
    Got from youtube.com and trimmed it down
    
    https://www.youtube.com/watch?v=Zm7Sbb9bzi8

--- Assets/Scripts/CollectiblePickup.cs
+++ Assets/Scripts/CollectiblePickup.cs
@@ -5,19 +5,8 @@ using UnityEngine;
 public class CollectiblePickup : MonoBehaviour
 {
     public int currentValue;
-
+    public AudioClip collectSound;
     public GameObject collectibleEffect;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,7 +14,14 @@ public class CollectiblePickup : MonoBehaviour
         {
             FindObjectOfType<GameManager>().AddCollectibles(currentValue);
 
+            // Instantiate the collectible effect
             Instantiate(collectibleEffect, transform.position, transform.rotation);
+
+            // Play the collect sound if available
+            if (collectSound != null)
+            {
+                AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            }
 
             Destroy(gameObject);
         }
