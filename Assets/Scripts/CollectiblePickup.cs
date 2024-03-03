73aa66919a0fe3fd0876e55a7f8c1e7bedf96b46
Peter Bui <peterbui27@gmail.com>
2024-03-03 16:55:26 -0500

    Added comments to edited scripts

--- Assets/Scripts/CollectiblePickup.cs
+++ Assets/Scripts/CollectiblePickup.cs
@@ -8,21 +8,26 @@ public class CollectiblePickup : MonoBehaviour
     public AudioClip collectSound;
     public GameObject collectibleEffect;
 
+
+    // on trigger, if "Player" (tagged player in hierarchy), touches collectible, then triggers this method
     private void OnTriggerEnter(Collider other)
     {
+        // checks for the Tag of "Player" of which we set in our hierarchy
         if (other.tag == "Player")
         {
+            // finds the collectible, and if collided then calls AddCollectibles in GameManager.
             FindObjectOfType<GameManager>().AddCollectibles(currentValue);
 
-            // Instantiate the collectible effect
+            // once pickup on the collectible, does collectible effect
             Instantiate(collectibleEffect, transform.position, transform.rotation);
 
-            // Play the collect sound if available
+            // once pickup on the collectible, does collectible sound effect
             if (collectSound != null)
             {
                 AudioSource.PlayClipAtPoint(collectSound, transform.position);
             }
 
+            // once pick up, destroys the collectible from the player's current session.
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     public static int currentCollectibles;
     public TextMeshProUGUI collectibleText;
     public TextMeshProUGUI winText;
+    // bool to check if the level is completed
     public static bool isLevelComplete;
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,13 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        // if the player collects 8 or if isLevelComplete is false
         if (currentCollectibles == 8 && !isLevelComplete)
         {
             LevelComplete();
         }
 
+        // if isLevelComplete is true, then give player option to press space to reset and play again
         if (isLevelComplete)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -35,16 +38,21 @@ public class GameManager : MonoBehaviour
     }
 
     public void AddCollectibles(int collectiblesToAdd)
-    {   
+    {
+        // counter for collectibles to track.
         currentCollectibles += collectiblesToAdd;
 
-        //update the call to collectible text ui
+        //update the call to collectible text ui everytime player collects collectible
         UpdateCollectibleText();
     }
+
+    // every time a player collects a collectible, update the text
     public void UpdateCollectibleText()
     {
+        // check if it exists
         if (collectibleText != null)
         {
+            // if exists, update the text out of 8 (max collectibles on checkpoint 1)
             collectibleText.text = "Steaks: " + currentCollectibles.ToString() + " / 8";
         }
         else
@@ -52,15 +60,20 @@ public class GameManager : MonoBehaviour
             Debug.LogWarning("Collectible does not exist!");
         }
     }
+
+    // once all 8 collectibles is completed, level complete is called
     public void LevelComplete()
     {
+        // stops the time, and shows the winText
         Time.timeScale = 0f;
         winText.enabled = true;
         winText.text = "You win! Thank you for playing Checkpoint 1! Press Space to play again!";
 
+        // sets the isLevelComplete to true as the level is completed
         isLevelComplete = true;
     }
 
+    // resets everything back to default
     private void Reset()
     {
         currentCollectibles = 0;
