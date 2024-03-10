c113935ecc81cb5c5dcbf73b75f46be7c5d11b70
Jimmy Nguyen <jim.ngu000@gmail.com>
2024-03-10 07:16:55 -0700

    Mostly working AI - Collision changes

--- Assets/Scripts/EnemyAITest2.cs
+++ Assets/Scripts/EnemyAITest2.cs
@@ -5,61 +5,93 @@ using UnityEngine.AI;
 
 public class EnemyAITest2 : MonoBehaviour
 {
-    public NavMeshAgent enemy;
-    public float waitTime = 4;
-    public float timeToRotate = 2;
-    public float walkSpeed = 6;
-    public float runSpeed = 9;
+    public float lookRadius = 10f;
+    public Transform[] waypoints;
+    int currWaypointIndex = 0;
 
-    public float viewRadius = 15;
-    public float viewAngle = 90;
-
-    public LayerMask playerMask;
-    public LayerMask groundMask;
-
-    public float meshRes = 1f;
-    public int edgeIterations = 4;
-    public float edgeDistance = 0.5f;
-    
-    Vector3 playerLastPos = Vector3.zero;
-    Vector3 playerPos;
-
-    public Transform[] destinations;
-    int currDestinIndex;
-
-    float m_waitTime;
-    float m_timeToRotate;
-    bool playerInRange;
-    bool playerNear;
-    bool patrolling;
-    bool playerCaught;
+    Transform target;
+    NavMeshAgent enemy;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerPos = Vector3.zero;
-        patrolling = true;
-        playerCaught = false;
-        playerInRange = false;
-        m_waitTime = waitTime;
-        m_timeToRotate = timeToRotate;
-
-        currDestinIndex = 0;
         enemy = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        enemy.speed = 3;
+        enemy.SetDestination(waypoints[currWaypointIndex].position);
+    }
 
-        enemy.isStopped = false;
-        enemy.speed = walkSpeed;
-        enemy.SetDestination(destinations[currDestinIndex].position);
+    // If we've reached a waypoint, move onto next waypoint
+    // Otherwise keep going to current waypoint
+    void Patrol()
+    {
+        enemy.speed = 3;
+        if (ReachedDestinationOrNot(enemy))
+        {
+            currWaypointIndex = (currWaypointIndex + 1) % waypoints.Length;
+            enemy.SetDestination(waypoints[currWaypointIndex].position);
+        } else
+        {
+            enemy.SetDestination(waypoints[currWaypointIndex].position);
+        }
+        //currWaypointIndex = (currWaypointIndex + 1) % waypoints.Length;
+    }
+
+    // 3 checks to see if enemy has reached their destination yet
+    bool ReachedDestinationOrNot(NavMeshAgent agent)
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Start chasing player
+    void Chase()
+    {
+        enemy.speed = 11;
+        enemy.SetDestination(target.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // If player is within lookRadius, start chasing
+        // Otherwise just patrol
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            Chase();
+            if (distance <= enemy.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        } else
+        {
+            Patrol();
+        }
     }
 
-    void CatchPlayer()
+    // Making sure character model faces player at all times
+    void FaceTarget()
     {
-        playerCaught = true;
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    // Draw lookRadius for debugging
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
