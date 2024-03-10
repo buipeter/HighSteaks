e1f9ce5dd86075057813a784b23ed9f48b141c89
Peter Bui <peterbui27@gmail.com>
2024-03-10 16:25:03 -0400

    Added collision to the Enemy AI

--- Assets/Scripts/HazardScript.cs
+++ Assets/Scripts/HazardScript.cs
@@ -35,9 +35,6 @@ public class HazardScript : MonoBehaviour
             {
                 AudioSource.PlayClipAtPoint(hitSound, transform.position);
             }
-
-            
         }
     }
-
 }
